dd43f5d2ff34b8e6a03fd89ee8cebb47a17b9aaf
LeinerViloria <vilorialeiner2002@gmail.com>
2024-05-27 22:36:40 -0500

    Ajuste

--- Backend/Configuration.Module.Controllers/CatalogueController/CatalogueController.cs
+++ Backend/Configuration.Module.Controllers/CatalogueController/CatalogueController.cs
@@ -3,20 +3,33 @@ using Configuration.Entities;
 using Appointment.SDK.Backend.Validations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace Appointment.Configuration.Controllers;
 
 public class CatalogueController(IServiceProvider serviceProvider) : StandardController<Catalogue, BaseControllerValidator<Catalogue>>(serviceProvider)
 {
-    [HttpGet("/getDataWithServices")]
+    [HttpGet("getDataWithServices")]
     public IActionResult GetDataWithServices()
     {
         using(var context = CreateContext())
         {
+            context.ChangeTracker.LazyLoadingEnabled = false;
             var Data = context.Set<Catalogue>()
                 .AsNoTracking()
                 .Include(x => x.Services)
-                .ToList();
+                .Select(x => new Catalogue(){
+                    Rowid = x.Rowid,
+                    Name = x.Name,
+                    Services = x.Services == null ? null : x.Services.Select(y => new Service(){
+                        Rowid = y.Rowid,
+                        Name = y.Name,
+                        Description = y.Description,
+                        Price = y.Price,
+                        RowidCatalogue = y.RowidCatalogue,
+                        Status = y.Status
+                    }).ToList()
+                }).ToList();
 
             return Ok(Data);
         }
